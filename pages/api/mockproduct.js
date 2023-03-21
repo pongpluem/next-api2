e77cbcfff396066e03012a28fd4fea3c44532015
Nattaporn  Pongpluem <pongpluem@hotmail.com>
2023-03-21 23:01:36 +0700

    firestore

---
+++ pages/api/mockproduct.js
@@ -0,0 +1,47 @@
+export default function handler(req, res) {
+   
+    const result = [
+        {
+            "id": '1000',
+            "code": 'f230fh0g3',
+            "name": 'Bamboo Watch',
+            "description": 'Product Description',
+            "image": 'bamboo-watch.jpg',
+            "price": 65,
+            "category": 'Accessories',
+            "quantity": 24,
+            "inventoryStatus": 'INSTOCK',
+            "rating": 5
+        },
+        {
+            "id": '1000',
+            "code": 'f230fh0g3',
+            "name": 'Bamboo Watch',
+            "description": 'Product Description',
+            "image": 'bamboo-watch.jpg',
+            "price": 65,
+            "category": 'Accessories',
+            "quantity": 24,
+            "inventoryStatus": 'INSTOCK',
+            "rating": 5
+        },
+        {
+            "id": '1000',
+            "code": 'f230fh0g3',
+            "name": 'Bamboo Watch',
+            "description": 'Product Description',
+            "image": 'bamboo-watch.jpg',
+            "price": 65,
+            "category": 'Accessories',
+            "quantity": 24,
+            "inventoryStatus": 'INSTOCK',
+            "rating": 5
+        },
+     
+    ]
+
+    console.log("Mock")
+
+    res.status(200).json( result )
+}
+
---
+++ pages/datatable3.js
@@ -0,0 +1,29 @@
+
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
+
+export default function BasicDemo() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/firestoreGetProduct")
+        .then((response) => response.json())
+        .then((data) => {            
+            console.log(data)          
+            setProducts(data)           
+        });
+    }, []);
+
+    return (
+        <div className="card">
+            <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="code" header="Code"></Column>
+                <Column field="name" header="Name"></Column>
+                <Column field="category" header="Category"></Column>
+                <Column field="quantity" header="Quantity"></Column>
+            </DataTable>
+        </div>
+    );
+}
